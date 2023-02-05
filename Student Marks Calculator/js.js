4fb66dd8655c1f1d4a8281fb570c30cad8381f11
Walaasalah123321 <walaasalah123321@gmail.com>
2023-02-05 20:29:48 +0200

    student mark

---
+++ Student Marks Calculator/js.js
@@ -0,0 +1,38 @@
+let text=document.querySelectorAll("input")
+console.log(text)
+let result=document.querySelector(".result")
+let par=document.querySelector(".par")
+result.onclick=function(){
+    let sum=0;
+    let avarge=0
+
+    text.forEach(item=>{
+        sum+=(+item.value);
+    })
+    avarge=sum/500*100;
+    if(avarge>=90){
+        flag="A you are pass "
+    }
+    else if (avarge>=80){
+        flag="B  you are pass "
+    }
+    else if (avarge>=70){
+        flag="C you are pass "
+    }
+    else if (avarge>=60){
+        flag="D you are pass "
+    }
+    else{
+        flag="F you are failed "
+    }
+    let ph=`
+    <p> your marks ${sum} out of 500</p>
+    <p> your percentage is ${avarge} %</p>
+    <p> your grade is ${flag} </p>
+
+    
+    `
+par.innerHTML=ph
+
+
+}
